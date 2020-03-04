0296159729444e42b9b2da1dd1e9311aa973f42a
Igor Artamonov <igor@artamonov.ru>
2020-03-03 21:31:24 -0500

    ci 2

---
+++ packages/native/jest-electron.config.js
@@ -0,0 +1,18 @@
+// Default test config doesn't include tests in src/__integration-tests__
+// to run them use
+// $ yarn workspace @emeraldpay/emerald-vault-native run jest:all
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    "<rootDir>/lib/",
+    "<rootDir>/node_modules/",
+    "__tests__/_commons",
+    "__integration-tests__"
+  ],
+  coverageDirectory: "./coverage/",
+  collectCoverageFrom: [
+    "src/**/*.ts"
+  ],
+  runner: '@jest-runner/electron/main',
+};
